f65d51cba3a3fbec37e5b36309a0638a06e94da3
Daniel Becker <dbecker.fl@gmail.com>
2019-06-29 09:25:43 -0700

    adding back in build script

--- .travis.d/build.sh
+++ .travis.d/build.sh
@@ -1,10 +1,9 @@
 echo "Travis Init"
 
-npm install
-
 echo "Setting up react..."
 # Installing the react cli
 npm install -g react-cli react
+npm install
 
 echo "Setting up AWS CLI..."
 pip3 install awscli --upgrade --user
