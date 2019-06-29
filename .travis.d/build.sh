8c2f9ecfc860c284580870d41dabe57e1cdcb052
Daniel Becker <dbecker.fl@gmail.com>
2019-06-29 08:58:22 -0700

    another attempt

--- .travis.d/build.sh
+++ .travis.d/build.sh
@@ -1,9 +1,10 @@
 echo "Travis Init"
 
+npm install
+
 echo "Setting up react..."
 # Installing the react cli
 npm install -g react-cli react
-npm install
 
 echo "Setting up AWS CLI..."
 pip3 install awscli --upgrade --user
