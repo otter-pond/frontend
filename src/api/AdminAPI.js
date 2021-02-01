e301e5400c06a8707083b50a5d9bfca80fd47c5a
Daniel Becker <dbecker.fl@gmail.com>
2021-01-31 19:06:32 -0500

    Add exception logging for semester launch

--- src/api/AdminAPI.js
+++ src/api/AdminAPI.js
@@ -4,6 +4,10 @@ export default class AdminAPI extends APIClient {
     launchSemester(launchConfig) {
         return new Promise((resolve, reject) => {
             this.perform("post", `/admin/semesterLaunch`, launchConfig).then(result => {
+                let exceptions = result["exceptions"]
+                if (exceptions && exceptions.length > 0) {
+                    console.error("Error(s) launching semester", exceptions)
+                }
                 resolve(result)
             }).catch(e => {
                 reject(e)
